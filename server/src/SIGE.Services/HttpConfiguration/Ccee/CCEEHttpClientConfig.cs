3f7df7dc4725bc9b174be499f876d08bb90f5ad1
Gabriel M Michels <gmaykot@gmail.com>
2024-11-28 22:42:26 -0300

    Att

--- server/src/SIGE.Services/HttpConfiguration/Ccee/CCEEHttpClientConfig.cs
+++ server/src/SIGE.Services/HttpConfiguration/Ccee/CCEEHttpClientConfig.cs
@@ -29,7 +29,7 @@ namespace SIGE.Services.HttpConfiguration.Ccee
                     if (!string.IsNullOrEmpty(cceeOptions?.CertificateValue) && !cceeOptions.CertificateValue.Equals("${CERTIFICATE_VALUE}"))
                     {
                         Console.WriteLine("##SUCCESS: Certificate Injected");
-                        Console.WriteLine("##SUCCESS: Certificate Options {0}", JsonConvert.SerializeObject(cceeOptions));
+                        //Console.WriteLine("##SUCCESS: Certificate Options {0}", JsonConvert.SerializeObject(cceeOptions));
                         byte[] certBytes = Convert.FromBase64String(cceeOptions.CertificateValue);
                         var certificatePem = new X509Certificate2(certBytes, cceeOptions.CertificatePass);
                         var certificadoCrt = X509Certificate2.CreateFromPemFile("/root/work/certificado.crt", "/root/work/chave.key");
