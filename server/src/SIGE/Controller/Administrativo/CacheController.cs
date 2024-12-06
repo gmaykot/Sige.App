67ef6c5015920fd26796253b506f4d343b7b0f03
Gabriel M Michels <gmaykot@gmail.com>
2024-12-06 16:26:47 -0300

    ClearCache Controller

---
+++ server/src/SIGE/Controller/Administrativo/CacheController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SIGE.Core.Cache;
+using SIGE.Core.Models.Defaults;
+using SIGE.Core.Models.Dto.Default;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace SIGE.Controller.Administrativo
+{
+    [ApiController]
+    [Route("cache")]
+    public class CacheController(ICacheManager cacheManager) : ControllerBase
+    {
+        private readonly ICacheManager _cacheManager = cacheManager;
+
+        [HttpPost("clear")]
+        [SwaggerOperation(Description = "Efetua a limpeza do cache no sistema.")]
+        [ProducesResponseType(typeof(Response), 200)]
+        [ProducesResponseType(typeof(Response), 400)]
+        [ProducesResponseType(typeof(Response), 401)]
+        [ProducesResponseType(typeof(Response), 500)]
+        public async Task<IActionResult> ClearCache([FromBody] DropDownDto? req)
+        {
+            if (req == null)
+                await _cacheManager.ClearAll();
+            else
+                await _cacheManager.Remove(req.Descricao);
+
+            return Ok();
+        }
+
+        [HttpGet("list-all")]
+        [SwaggerOperation(Description = "Obtém as chaves do cache no sistema.")]
+        [ProducesResponseType(typeof(Response), 200)]
+        [ProducesResponseType(typeof(Response), 400)]
+        [ProducesResponseType(typeof(Response), 401)]
+        [ProducesResponseType(typeof(Response), 500)]
+        public async Task<IActionResult> ListAllKeys()
+        {
+            return Ok(await _cacheManager.ListAllKeys());
+        }
+    }
+}
--- web/src/app/@core/services/administrativo/cache.service.ts
+++ web/src/app/@core/services/administrativo/cache.service.ts
@@ -4,17 +4,17 @@ import { IDropDown } from "../../data/drop-down";
 
 @core.Injectable({ providedIn: "root" })
 export class CacheService {
-    private urlBase = "auth";
+    private urlBase = "cache";
 
     constructor(private http: HttpService) {
     }
 
     public async listCache(): Promise<string[]> {
-        return await this.http.get<string[]>(`/${this.urlBase}/list-cache`
+        return await this.http.get<string[]>(`/${this.urlBase}/list-all`
     );
     }
 
     public async clearCache(req?: IDropDown): Promise<any> {
-        return await this.http.post<any>(`/${this.urlBase}/clear-cache`, req);   
+        return await this.http.post<any>(`/${this.urlBase}/clear`, req);   
     }
 }
