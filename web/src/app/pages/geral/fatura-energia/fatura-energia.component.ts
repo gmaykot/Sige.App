5ccf40444b5fe9e4cf4bbccded76c4211f707f79
Luciane Santos <lucianesantcs@gmail.com>
2025-03-28 14:50:24 -0300

    feat: configurado formulário e adicionado métodos faltantes

--- web/src/app/pages/geral/fatura-energia/fatura-energia.component.ts
+++ web/src/app/pages/geral/fatura-energia/fatura-energia.component.ts
@@ -1,17 +1,46 @@
-import { Component } from '@angular/core';
-import { IDropDown } from '../../../@core/data/drop-down';
-import { LocalDataSource } from 'ng2-smart-table';
+import { Component } from "@angular/core";
+import { IDropDown } from "../../../@core/data/drop-down";
+import { LocalDataSource } from "ng2-smart-table";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
-  selector: 'ngx-fatura-energia',
-  templateUrl: './fatura-energia.component.html',
-  styleUrls: ['./fatura-energia.component.scss']
+  selector: "ngx-fatura-energia",
+  templateUrl: "./fatura-energia.component.html",
+  styleUrls: ["./fatura-energia.component.scss"],
 })
 export class FaturaEnergiaComponent {
-  public empresas: Array<IDropDown> = []
-  public concessionarias: Array<IDropDown> = []
+  public empresas: Array<IDropDown> = [];
+  public concessionarias: Array<IDropDown> = [];
   public editLabel: string = null;
   public source: LocalDataSource = new LocalDataSource();
+
+  public control = this.formBuilder.group({
+    concessionariaId: [null],
+    mesReferencia: [null],
+    dataVencimento: [null],
+    empresas: [null],
+    demPta: [null],
+    demForaPta: [null],
+    demPtaFaturada: [null],
+    demForaPtaFaturada: [null],
+    demForaPtaUltrapassagem: [null],
+    demPtaReativa: [null],
+    demForaPtaReativa: [null],
+    consPtaMedioTusd: [null],
+    consForaPtaMedioTusd: [null],
+    consForaPtaMedioTe: [null],
+    adBandTarVigPta: [null],
+    adBandTarVigFPta: [null],
+    consPtaMedidoReativo: [null],
+    consForaPtaMedidoReativo: [null],
+    subvencaoTarifaria: [null]
+  });
+
+  public lancamentoControl = this.formBuilder.group({
+    lancamento: [null],
+    valor: [null],
+    tipo: [null]
+  })
 
   settings = {
     actions: {
@@ -21,26 +50,39 @@ export class FaturaEnergiaComponent {
     },
     columns: {
       empresa: {
-        title: 'Lançamento',
-        type: 'string',
+        title: "Lançamento",
+        type: "string",
       },
       concessionaria: {
-        title: 'Valor (R$)',
-        type: 'string',
+        title: "Valor (R$)",
+        type: "string",
       },
       valor: {
-        title: 'Tipo',
-        type: 'string',
+        title: "Tipo",
+        type: "string",
       },
     },
     hideSubHeader: true,
     noDataMessage: "Nenhum registro encontrado.",
-  }
-  
+  };
+
+  constructor(private formBuilder: FormBuilder) {}
+
   onItemSelected(selectedItem: IDropDown) {
+    if (selectedItem) {
+      this.control.get("empresas").setValue(selectedItem?.descricao);
+    }
   }
 
   selecionarColeta(event) {
     
+  }
+
+  emitirFatura() {
+      console.log("Emitindo fatura com os dados:", this.control.value);
+  }
+
+  adicionarLancamento() {
+    console.log("Adicionado lançamentos:", this.lancamentoControl.value);
   }
 }
