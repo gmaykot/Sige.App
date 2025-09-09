9bc2417f457d6c9ceef8da27bc59862960c932e3
Gabriel M Michels <gmaykot@gmail.com>
2025-09-09 09:10:15 -0300

    group by

--- server/src/SIGE.Services/Services/Externo/EmailService.cs
+++ server/src/SIGE.Services/Services/Externo/EmailService.cs
@@ -194,10 +194,10 @@ namespace SIGE.Services.Services.Externo {
             var res = await _appDbContext.LogsEnvioEmails.Include(l => l.UsuarioEnvio).Include(l => l.RelatorioMedicao).ThenInclude(r => r.Contrato).OrderByDescending(l => l.RelatorioMedicao.MesReferencia).ToListAsync();
             if (res != null) {
                 var resultado = res
-                    .GroupBy(a => a.RelatorioMedicao.MesReferencia)
+                    .GroupBy(a => a.RelatorioMedicao?.MesReferencia)
                     .Select(g => new {
                         data = new {
-                            mesReferencia = g.Key.ToString("MM/yyyy"),
+                            mesReferencia = g.Key.Value.ToString("MM/yyyy"),
                             tipo = "group",
                             qtdItens = g.Count()
                         },
