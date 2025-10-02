fe8f7354aca7c876e3b2a0d6025ec90a950fbb34
Gabriel M Michels <gmaykot@gmail.com>
2025-10-02 13:39:09 -0300

    Atualização pacotes

--- web/src/app/@core/guards/GeralGuard.ts
+++ web/src/app/@core/guards/GeralGuard.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, Router } from "@angular/router";
+import { Router } from "@angular/router";
 import { SessionStorageService } from "../services/util/session-storage.service";
 
 @Injectable({ providedIn: 'root' })
-export class GeralGuard implements CanActivate {
+export class GeralGuard  {
     constructor(private router: Router) {}
 
     canActivate(): boolean {
--- web/src/app/@core/guards/SAGuard.ts
+++ web/src/app/@core/guards/SAGuard.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, Router } from "@angular/router";
+import { Router } from "@angular/router";
 import { SessionStorageService } from "../services/util/session-storage.service";
 
 @Injectable({ providedIn: 'root' })
-export class SAGuard implements CanActivate {
+export class SAGuard  {
     constructor(private router: Router) {}
 
     canActivate(): boolean {
